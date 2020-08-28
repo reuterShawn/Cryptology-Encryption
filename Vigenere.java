60bd6f7b459ca521b9c7f2fb893305983d31f8a9
Joe <deglm006@morris.umn.edu>
2020-08-28 17:36:19 -0500

    Write splitText method

--- Vigenere.java
+++ Vigenere.java
@@ -8,9 +8,19 @@ public class Vigenere {
 
     String encrypt(String plaintext){
         Ceasar[] ciphers = new Ceasar[key.length()];
-        String[] texts = new String[key.length()];
+        String[] texts = splitText(plaintext);
+
     }
 
-
+    String[] splitText(String text){
+        String[] texts = new String[key.length()];
+        int i=0;
+        while (!text.equals("")){
+            texts[i] += text.substring(0,1);
+            text = text.substring(1);
+            i= (i+1) % key.length();
+        }
+        return texts;
+    }
 
 }
