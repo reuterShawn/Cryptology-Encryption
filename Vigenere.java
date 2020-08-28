79bbbb26776bebcb8c9b47a3805c9c237f246540
Joe <deglm006@morris.umn.edu>
2020-08-28 17:23:41 -0500

    Create Vigenere class

---
+++ Vigenere.java
@@ -0,0 +1,16 @@
+public class Vigenere {
+    String key;
+    int[] keyArr;
+
+    Vigenere(){
+
+    }
+
+    String encrypt(String plaintext){
+        Ceasar[] ciphers = new Ceasar[key.length()];
+        String[] texts = new String[key.length()];
+    }
+
+
+
+}
