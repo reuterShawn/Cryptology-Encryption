11e84276ce8666ab27d05f3dd219634f6249e9f4
reuterShawn <60119326+reuterShawn@users.noreply.github.com>
2020-08-28 16:42:56 -0500

    Create encryption-decryption.java

---
+++ encryption-decryption.java
@@ -0,0 +1,30 @@
+public class encrpt {
+
+
+    public String takeStuff ( String text, String keyword){
+
+       String  filteredText = filterText(text);
+       int size = keyword.length();
+       int[] keywordSize = new int[size];
+    }
+
+    public int[] takeString ( String word){
+        int wordSize =word.length();
+        for (int i = 0; i < wordSize ; i++; ) {
+            String letter = word.substring(i, i+1);
+            takeLetter(letter);
+        }
+    }
+    public int takeLetter ( String letter){
+
+        String alphabet = "ABCDEFGHJIKLMNOPQRSTUVWXYZ";
+        int index = alphabet.indexOf(alphabet);
+        return index;
+    }
+    public static String filterText (String text){
+        text = text.toUpperCase();
+        String allLetters = text.replaceAll("[^A-Z]", " ");
+        return allLetters;
+    }
+
+}
